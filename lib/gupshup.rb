d7943890f9facbbecad5c55f6c3927daa8c3a640
Nilesh Trivedi <github@nileshtrivedi.com>
2010-03-25 18:36:26 +0530

    Refactored the code. Added group_post and a generic call_api method.

--- lib/gupshup.rb
+++ lib/gupshup.rb
@@ -6,42 +6,40 @@ require 'uri'
 require 'cgi'
 require 'httpclient'
 
+class NilClass
+  def blank?
+    true
+  end
+end
+
+class String
+  def blank?
+    self.empty?
+  end
+end
+
 module Gupshup
-  VERSION = '0.1.2'
+  VERSION = '0.1.5'
   class Enterprise
-    def initialize(login,password, token = nil)
+    def initialize(opts)
       @api_url = 'http://enterprise.smsgupshup.com/GatewayAPI/rest'
       @api_params = {}
-      @api_params[:userid] = login
-      @api_params[:password] = password
-      @api_params[:v] = '1.1'
-      @api_params[:auth_scheme] = 'PLAIN'
-      if token
+      @api_params[:userid] = opts[:userid]
+      @api_params[:password] = opts[:password]
+      @api_params[:v] = opts[:v] || '1.1'
+      @api_params[:auth_scheme] = opts[:auth_scheme] || 'PLAIN'
+      unless opts[:token].blank?
         @api_params[:auth_scheme] = 'TOKEN'
-        @api_params[:token] = token
+        @api_params[:token] = opts[:token]
         @api_params.delete(:password)
       end
+      raise "Invalid credentials" if opts[:userid].blank? || (opts[:password].blank? && opts[:token].blank?)
     end
-
-    def send_message(msg,number,msg_type = 'TEXT',opts = {})
-      raise 'Phone Number is too short' if number.to_s.length < 12
-      raise 'Phone Number is too long' if number.to_s.length > 12
-      #raise 'Phone Number should start with "91"' if number.to_s.start_with? "91"
-      raise 'Phone Number should be numerical value' unless number.to_i.to_s == number.to_s
-      raise 'Message should be less than 725 characters long' if msg.to_s.length > 724
-      msg_params = {}
-      msg_params[:method] = 'sendMessage'
-      msg_params[:msg_type] = msg_type.to_s
-      msg_params[:msg] = msg.to_s
-      msg_params[:send_to] = number.to_s
-      #url = URI.parse(@api_url)
-      #req = Net::HTTP::Post.new(url.path)
-      #puts "--- #{msg_params.merge(@api_params).inspect}"
-      #req.set_form_data(msg_params.merge(@api_params))
-      #res = Net::HTTP.new(url.host, url.port).start {|http|http.request(req) }
+    
+    def call_api(opts = {})
       res = Net::HTTP.post_form(
         URI.parse(@api_url),
-        msg_params.merge(@api_params).merge(opts)
+        @api_params.merge(opts)
       )
       resp = res.body
       puts "GupShup Response: #{resp}"
@@ -49,7 +47,7 @@ module Gupshup
       case res
       when Net::HTTPSuccess, Net::HTTPRedirection
         if resp.nil? || resp.include?("success") == false
-          raise "SMS Sending failed: #{resp}"
+          raise "API call '#{opts[:method]}' failed: #{resp}"
         end
         return true
       else
@@ -57,20 +55,33 @@ module Gupshup
       end
     end
 
-    def send_flash_message(msg,number,opts = {})
-      send_message(msg,number,:FLASH,opts)
+    def send_message(opts)
+      msg = opts[:msg]
+      number = opts[:send_to]
+      msg_type = opts[:msg_type] || 'TEXT'
+      
+      raise 'Phone Number is too short' if number.to_s.length < 12
+      raise 'Phone Number is too long' if number.to_s.length > 12
+      #raise 'Phone Number should start with "91"' if number.to_s.start_with? "91"
+      raise 'Phone Number should be numerical value' unless number.to_i.to_s == number.to_s
+      raise 'Message should be less than 725 characters long' if msg.to_s.length > 724
+      call_api opts.merge({ :method => 'sendMessage' })
     end
 
-    def send_text_message(msg,number,opts = {})
-      send_message(msg,number,:TEXT,opts)
+    def send_flash_message(opts)
+      send_message(opts.merge({ :msg_type => 'FLASH'}))
     end
 
-    def send_vcard(card,number,opts = {})
-      send_message(card,number,:VCARD,opts)
+    def send_text_message(opts)
+      send_message(opts.merge({ :msg_type => 'TEXT'}))
     end
 
-    def send_unicode_message(msg,number,opts = {})
-      send_message(msg,number,:UNICODE_TEXT,opts)
+    def send_vcard(opts)
+      send_message(opts.merge({ :msg_type => 'VCARD'}))
+    end
+
+    def send_unicode_message(opts)
+      send_message(opts.merge({ :msg_type => 'UNICODE_TEXT'}))
     end
 
     def bulk_file_upload(file_path,file_type = 'csv',mime_type = 'text/csv', opts = {})
@@ -83,6 +94,13 @@ module Gupshup
       resp = HTTPClient.post(@api_url,msg_params.merge(@api_params).merge(opts))
       file.close
       puts resp.body.content
+    end
+    
+    def group_post(opts)
+      raise "Invalid group name" if opts[:group_name].blank?
+      raise "Invalid message" if opts[:msg].blank?
+      raise "Invalid message type" if opts[:msg_type].blank?
+      call_api opts.merge({:method => 'post_group'})
     end
   end
 end
